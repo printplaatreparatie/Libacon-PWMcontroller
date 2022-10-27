5ef01fcb055190cecca942f554ed0868d5b5076d
MA.vanEngelenhoven <MA.vanEngelenhoven@student.han.nl>
2022-10-27 12:16:56 +0200

    main code layout done, and intervals decided

--- include/LIMACH.h
+++ include/LIMACH.h
@@ -8,3 +8,34 @@ int voltage2PWM(int voltage, int dither);
 int calculateDither(int dither);
 void writePWM9123(bool state);
 void writePWM9145_46(bool state, bool output);
+
+#define joyL 10    //FX1056
+#define joyR 9     //FX1057
+#define button 3   //SW3604
+#define MA9136 0   //K1
+#define PWM9145 5  //OUT2(U4,Q2)
+#define PWM9146 6  //OUT3(U5,Q4)
+#define PWM9123 4  //OUT1(U1,Q9)
+#define MA9190 1   //K2
+
+unsigned long previousMicrosJoyL = 0;
+const long intervalJoyL = 500;
+
+unsigned long previousMicrosJoyR = 0;
+const long intervalJoyR = 500;
+
+unsigned long previousMicrosDither = 0;
+const long intervalDither = 5000;
+
+unsigned long previousMicrosButton = 0;
+const long intervalButton = 10000;
+
+unsigned long previousMicrosPWM9123ON = 0;
+const long intervalPWM9123ON = 250;
+
+long intervalPWM9123OFF = 125;
+
+unsigned long previousMicrosPWM9145_46ON = 0;
+const long intervalPWM9145_46ON = 250;
+
+long intervalPWM9145_46OFF = 125;
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
 #include "LIMACH.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  pinMode(joyL, INPUT);
+  pinMode(joyR, INPUT);
+
+  pinMode(MA9136, OUTPUT);
+  pinMode(MA9190, OUTPUT);
+  pinMode(PWM9123, OUTPUT);
+  pinMode(PWM9145, OUTPUT);
+  pinMode(PWM9146, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  unsigned long currentMillis = micros();
+
+  if (currentMillis - previousMicrosPWM9123ON >= intervalPWM9123ON)
+  {
+  }
+
+  if (currentMillis - previousMicrosPWM9123ON >= intervalPWM9123OFF)
+  {
+  }
+
+  if (currentMillis - previousMicrosPWM9145_46ON >= intervalPWM9145_46ON)
+  {
+  }
+
+  if (currentMillis - previousMicrosPWM9145_46ON >= intervalPWM9145_46OFF)
+  {
+  }
+
+  if (currentMillis - previousMicrosJoyL >= intervalJoyL)
+  {
+  }
+
+  if (currentMillis - previousMicrosDither >= intervalDither)
+  {
+  }
+
+  if (currentMillis - previousMicrosJoyR >= intervalJoyR)
+  {
+  }
+
+  if (currentMillis - previousMicrosButton >= intervalButton)
+  {
+  }
 }
