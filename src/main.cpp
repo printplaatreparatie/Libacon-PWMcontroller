32eda363c7464fc1941968d3da0a4d4055c92cf9
MA.vanEngelenhoven <MA.vanEngelenhoven@student.han.nl>
2022-11-04 12:26:24 +0100

    change out1 to OC0A pin 8. and use PWM registers instead of analogWrite or bitbashing

--- src/main.cpp
+++ src/main.cpp
@@ -10,30 +10,30 @@ int calculateDither(int dither);           // update the dither value(this value
 #define MA9136 0  // K1
 #define PWM9145 5 // OUT2(U4,Q2)
 #define PWM9146 6 // OUT3(U5,Q4)
-#define PWM9123 4 // OUT1(U1,Q9)
+#define PWM9123 8 // OUT1(U1,Q9)
 #define MA9190 1  // K2
 
 int ditherRange = 10; // dither range in percentage around the PWM dutycycle(so ditherRange = 10 & PWM = 50% => PWM = 45%-55%)
 
-unsigned long previousMicrosJoyL = 0;
-const long intervalJoyL = 500;
+// unsigned long previousMicrosJoyL = 0;
+// const long intervalJoyL = 10000;
 
-unsigned long previousMicrosJoyR = 0;
-const long intervalJoyR = 500;
+// unsigned long previousMicrosJoyR = 0;
+// const long intervalJoyR = 10000;
 
-unsigned long previousMicrosDither = 0;
-const long intervalDither = 5000;
+// unsigned long previousMicrosDither = 0;
+// const long intervalDither = 5000;
 
-unsigned long previousMicrosButton = 0;
-const long intervalButton = 25000;
+// unsigned long previousMicrosButton = 0;
+// const long intervalButton = 25000;
 
-const long intervalPWM = 250;
-unsigned long previousMicrosPWM9123ON = 0;
-unsigned long previousMicrosPWM9145_46ON = 0;
+// const long intervalPWM = 250;
+// unsigned long previousMicrosPWM9123ON = 0;
+// unsigned long previousMicrosPWM9145_46ON = 0;
 
-unsigned long intervalPWM9123OFF = intervalPWM;
+// unsigned long intervalPWM9123OFF = intervalPWM;
 
-unsigned long intervalPWM9145_46OFF = intervalPWM;
+// unsigned long intervalPWM9145_46OFF = intervalPWM;
 
 void setup()
 {
@@ -45,6 +45,17 @@ void setup()
   pinMode(PWM9123, OUTPUT);
   pinMode(PWM9145, OUTPUT);
   pinMode(PWM9146, OUTPUT);
+
+  TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(COM1B1);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+
+  TCCR0A = _BV(COM0A1) | _BV(WGM00);
+  TCCR0B = _BV(CS01);
+
+  OCR1A = 0;
+  OCR1B = 0;
+
+  OCR0A = 0;
 }
 
 void loop()
@@ -52,79 +63,79 @@ void loop()
   static int dither = 0;
   static bool polarity9190 = 0;
   static bool polarity45_46 = 0;
-  unsigned long currentMicros = micros();
-  unsigned long previousMicros = 0;
-  unsigned long microsAlowwedToRunLoop = 25;
+  // unsigned long currentMicros = micros();
+  // unsigned long previousMicros = 0;
+  //  unsigned long microsAlowwedToRunLoop = 75;
 
-  if (currentMicros - previousMicrosPWM9123ON >= intervalPWM)
-  {
-    previousMicrosPWM9123ON = currentMicros;
-    digitalWrite(PWM9123, HIGH);
+  // if (currentMicros - previousMicrosPWM9123ON >= intervalPWM)
+  // {
+  //   previousMicrosPWM9123ON = currentMicros;
+  //   digitalWrite(PWM9123, HIGH);
     if (polarity9190)
     {
-      //digitalWrite(MA9190, HIGH);
+      // digitalWrite(MA9190, HIGH);
     }
     else
     {
-      //digitalWrite(MA9190, LOW);
+      // digitalWrite(MA9190, LOW);
     }
-  }
+  // }
 
-  if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM)
-  {
-    previousMicrosPWM9145_46ON = currentMicros;
-    digitalWrite(PWM9145, HIGH);
-    digitalWrite(PWM9146, HIGH);
-  }
+  // if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM)
+  // {
+  //   previousMicrosPWM9145_46ON = currentMicros;
+  //   digitalWrite(PWM9145, HIGH);
+  //   digitalWrite(PWM9146, HIGH);
+  // }
 
-  if (currentMicros - previousMicrosPWM9123ON >= intervalPWM9123OFF)
-  {
-    digitalWrite(PWM9123, LOW);
-  }
+  // if (currentMicros - previousMicrosPWM9123ON >= intervalPWM9123OFF)
+  // {
+  //   digitalWrite(PWM9123, LOW);
+  // }
 
-  if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM9145_46OFF)
-  {
-    if (polarity45_46)
-    {
-      digitalWrite(PWM9145, LOW);
-    }
-    else
-    {
-      digitalWrite(PWM9146, LOW);
-    }
-  }
+  // if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM9145_46OFF)
+  // {
+  //   if (polarity45_46)
+  //   {
+  //     digitalWrite(PWM9145, LOW);
+  //   }
+  //   else
+  //   {
+  //     digitalWrite(PWM9146, LOW);
+  //   }
+  // }
 
-  if (currentMicros - previousMicrosDither >= intervalDither)
-  {
-    dither = calculateDither(dither);
-  }
+  // if (currentMicros - previousMicrosDither >= intervalDither)
+  // {
+  //   dither = calculateDither(dither);
+  // }
 
-  if (currentMicros - previousMicrosJoyL >= intervalJoyL)
-  {
-    polarity45_46 = analog2PWM(joyL, dither);
-  }
+  // if (currentMicros - previousMicrosJoyL >= intervalJoyL)
+  // {
+  //   polarity45_46 = analog2PWM(joyL, dither);
+  // }
 
-  if (currentMicros - previousMicrosJoyR >= intervalJoyR)
-  {
-    polarity9190 = analog2PWM(joyR, dither);
-  }
+  // if (currentMicros - previousMicrosJoyR >= intervalJoyR)
+  // {
+  //   polarity9190 = analog2PWM(joyR, dither);
+  // }
 
-  if (currentMicros - previousMicrosButton >= intervalButton)
-  {
-    Button();
-  }
+  // if (currentMicros - previousMicrosButton >= intervalButton)
+  // {
+  //   Button();
+  // }
 
-  previousMicros = micros();
-  if ((previousMicros - currentMicros) > microsAlowwedToRunLoop)
-  {
-    digitalWrite(MA9136, HIGH);
-    digitalWrite(MA9190, HIGH);
-  }
-  else
-  {
-    digitalWrite(MA9136, LOW);
-    digitalWrite(MA9190, LOW);
-  }
+  // previousMicros = micros();
+  // if ((previousMicros - currentMicros) > microsAlowwedToRunLoop)
+  // {
+  //   digitalWrite(MA9136, HIGH);
+  //   digitalWrite(MA9190, HIGH);
+  // }
+  // else
+  // {
+  //   digitalWrite(MA9136, LOW);
+  //   digitalWrite(MA9190, LOW);
+  // }
 }
 
 int calculateDither(int dither)
@@ -147,7 +158,7 @@ void Button(void)
     if (loopsPassed == 1) // debounce (if the button has been pressed for 2 loops(so 2*buttonInterval) the button is truly pressed)
     {
       MA9136State = !MA9136State;
-      //digitalWrite(MA9136, MA9136State);
+      digitalWrite(MA9136, MA9136State);
     }
   }
   else
@@ -158,13 +169,13 @@ void Button(void)
 
 bool analog2PWM(int whichADC, int dither)
 {
-  //const int error = 10;
-  //const int maxValue = 512 - error;
-  //const int minValue = 0 + error;
-  //int delayToTurnOff = intervalPWM;
+  // const int error = 10;
+  // const int maxValue = 512 - error;
+  // const int minValue = 0 + error;
+  // // int delayToTurnOff = intervalPWM;
   bool polarity = false;
-  dither = ditherRange / 2;
-  //int analogValue = analogRead(whichADC);
+  // dither = ditherRange / 2;
+  // int analogValue = analogRead(whichADC);
   // if (analogValue >= 512)
   // {
   //   polarity = true;
