5cbd720337bc13742ef71873732179259e75a9cc
MA.vanEngelenhoven <MA.vanEngelenhoven@student.han.nl>
2022-11-04 16:30:13 +0100

    analog2PWM works now

--- src/main.cpp
+++ src/main.cpp
@@ -13,27 +13,9 @@ int calculateDither(int dither);           // update the dither value(this value
 #define PWM9123 8 // OUT1(U1,Q9)
 #define MA9190 1  // K2
 
-int ditherRange = 10; // dither range in percentage around the PWM dutycycle(so ditherRange = 10 & PWM = 50% => PWM = 45%-55%)
-
-// unsigned long previousMicrosJoyL = 0;
-// const long intervalJoyL = 10000;
-
-// unsigned long previousMicrosJoyR = 0;
-// const long intervalJoyR = 10000;
-
-// unsigned long previousMicrosDither = 0;
-// const long intervalDither = 5000;
-
-// unsigned long previousMicrosButton = 0;
-// const long intervalButton = 25000;
-
-// const long intervalPWM = 250;
-// unsigned long previousMicrosPWM9123ON = 0;
-// unsigned long previousMicrosPWM9145_46ON = 0;
-
-// unsigned long intervalPWM9123OFF = intervalPWM;
-
-// unsigned long intervalPWM9145_46OFF = intervalPWM;
+int ditherRange = 20; // dither range in percentage around the PWM dutycycle(so ditherRange = 10 & PWM = 50% => PWM = 45%-55%)
+int dutyCycle_4546 = 0;
+int dutyCycle_9123 = 0;
 
 void setup()
 {
@@ -46,16 +28,11 @@ void setup()
   pinMode(PWM9145, OUTPUT);
   pinMode(PWM9146, OUTPUT);
 
-  TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(COM1B1);
+	TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(COM1B1);
   TCCR1B = _BV(CS11) | _BV(WGM12);
 
   TCCR0A = _BV(COM0A1) | _BV(WGM00);
   TCCR0B = _BV(CS01);
-
-  OCR1A = 0;
-  OCR1B = 0;
-
-  OCR0A = 0;
 }
 
 void loop()
@@ -63,86 +40,51 @@ void loop()
   static int dither = 0;
   static bool polarity9190 = 0;
   static bool polarity45_46 = 0;
-  // unsigned long currentMicros = micros();
-  // unsigned long previousMicros = 0;
-  //  unsigned long microsAlowwedToRunLoop = 75;
+  Button();
+  dither = calculateDither(dither);
+  polarity9190 = analog2PWM(joyL, dither);
+  polarity45_46 = analog2PWM(joyR, dither);
 
-  // if (currentMicros - previousMicrosPWM9123ON >= intervalPWM)
-  // {
-  //   previousMicrosPWM9123ON = currentMicros;
-  //   digitalWrite(PWM9123, HIGH);
-    if (polarity9190)
-    {
-      // digitalWrite(MA9190, HIGH);
-    }
-    else
-    {
-      // digitalWrite(MA9190, LOW);
-    }
-  // }
-
-  // if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM)
-  // {
-  //   previousMicrosPWM9145_46ON = currentMicros;
-  //   digitalWrite(PWM9145, HIGH);
-  //   digitalWrite(PWM9146, HIGH);
-  // }
-
-  // if (currentMicros - previousMicrosPWM9123ON >= intervalPWM9123OFF)
-  // {
-  //   digitalWrite(PWM9123, LOW);
-  // }
-
-  // if (currentMicros - previousMicrosPWM9145_46ON >= intervalPWM9145_46OFF)
-  // {
-  //   if (polarity45_46)
-  //   {
-  //     digitalWrite(PWM9145, LOW);
-  //   }
-  //   else
-  //   {
-  //     digitalWrite(PWM9146, LOW);
-  //   }
-  // }
-
-  // if (currentMicros - previousMicrosDither >= intervalDither)
-  // {
-  //   dither = calculateDither(dither);
-  // }
-
-  // if (currentMicros - previousMicrosJoyL >= intervalJoyL)
-  // {
-  //   polarity45_46 = analog2PWM(joyL, dither);
-  // }
-
-  // if (currentMicros - previousMicrosJoyR >= intervalJoyR)
-  // {
-  //   polarity9190 = analog2PWM(joyR, dither);
-  // }
-
-  // if (currentMicros - previousMicrosButton >= intervalButton)
-  // {
-  //   Button();
-  // }
-
-  // previousMicros = micros();
-  // if ((previousMicros - currentMicros) > microsAlowwedToRunLoop)
-  // {
-  //   digitalWrite(MA9136, HIGH);
-  //   digitalWrite(MA9190, HIGH);
-  // }
-  // else
-  // {
-  //   digitalWrite(MA9136, LOW);
-  //   digitalWrite(MA9190, LOW);
-  // }
+  OCR0A = dutyCycle_9123;
+  if (polarity9190)
+  {
+    digitalWrite(MA9190, HIGH);
+  }
+  else
+  {
+    digitalWrite(MA9190, LOW);
+  }
+  if (polarity45_46)
+  {
+    OCR1A = dutyCycle_4546;
+    //OCR1B = 255;
+  }
+  else
+  {
+    OCR1B = dutyCycle_4546;
+    //OCR1A = 255;
+  }
 }
 
 int calculateDither(int dither)
 {
-  if (dither <= ditherRange)
+  static bool increase = true;
+  if (dither >= ditherRange)
+  {
+    increase = false;
+  }
+  if (dither <= 0)
+  {
+    increase = true;
+  }
+
+  if (increase)
   {
     dither++;
+  }
+  else
+  {
+    dither--;
   }
   return dither;
 }
@@ -155,7 +97,7 @@ void Button(void)
   if (!(digitalRead(button))) // if the button is LOW(pushed)
   {
     loopsPassed++;
-    if (loopsPassed == 1) // debounce (if the button has been pressed for 2 loops(so 2*buttonInterval) the button is truly pressed)
+    if (loopsPassed == 10) // debounce (if the button has been pressed for n loops(the button is truly pressed)
     {
       MA9136State = !MA9136State;
       digitalWrite(MA9136, MA9136State);
@@ -169,47 +111,48 @@ void Button(void)
 
 bool analog2PWM(int whichADC, int dither)
 {
-  // const int error = 10;
-  // const int maxValue = 512 - error;
-  // const int minValue = 0 + error;
-  // // int delayToTurnOff = intervalPWM;
+  const long maxValue = 511 - 10;
+  const long minValue = 0 + 165;
+  const long maxPWM = 255;
+  const long minPWM = 0;
+  long dutyCycle = 0;
+
   bool polarity = false;
-  // dither = ditherRange / 2;
-  // int analogValue = analogRead(whichADC);
-  // if (analogValue >= 512)
-  // {
-  //   polarity = true;
-  //   analogValue = 512 - (analogValue - 512);
-  // }
+  long analogValue = (long)analogRead(whichADC);
+  if (analogValue >= 512)
+  {
+    polarity = true;
+    analogValue = 511 - (analogValue - 512);
+  }
 
-  // if (analogValue < minValue)
-  // {
-  //   delayToTurnOff = 0;
-  // }
-  // else if (analogValue > maxValue)
-  // {
-  //   delayToTurnOff = intervalPWM;
-  // }
-  // else
-  // {
-  //   analogValue = analogValue - minValue;
-  //   int dutycycle = (((analogValue * 100) / maxValue) + (ditherRange / 2) - dither);
-  //   delayToTurnOff = intervalPWM * (dutycycle / 100);
-  // }
+  if (analogValue < minValue)
+  {
+    dutyCycle = minPWM;
+  }
+  else if (analogValue > maxValue)
+  {
+    dutyCycle = maxPWM;
+  }
+  else
+  {
+      dutyCycle = (((analogValue-minValue)*maxPWM)/(maxValue-minValue));
 
-  // switch (whichADC)
-  // {
-  // case joyL:
-  //   intervalPWM9123OFF = delayToTurnOff;
-  //   break;
+  //   // analogValue = analogValue - minValue;
+  //   // double dutycyclePercentage = ((analogValue * 100) / 512); //(maxValue - minValue));
+  //   // dutyCycle = ((dutycyclePercentage * maxPWM) / 100);
 
-  // case joyR:
-  //   intervalPWM9145_46OFF = delayToTurnOff;
-  //   break;
-
-  // default:
-  //   break;
-  // }
-
+  //   //+ (ditherRange / 2) - dither)
+  }
+  switch (whichADC)
+  {
+  case joyL:
+    dutyCycle_9123 = dutyCycle;
+    break;
+  case joyR:
+    dutyCycle_4546 = dutyCycle;
+    break;
+  default:
+    break;
+  }
   return polarity;
 }
