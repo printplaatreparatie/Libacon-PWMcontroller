49f918eb051eec608335fd01d2e65a2e7e36d46c
MA.vanEngelenhoven <MA.vanEngelenhoven@student.han.nl>
2022-11-05 17:23:07 +0100

    CODE FINISHED/added the last comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include "Arduino.h"
 
-void Button(void);                         // read the button on the left joystick and if pushed toggle MA9136
+void Button(void);                         // read the button on the left joystick and if pushed turn on MA9136
 bool analog2PWM(int whichADC, int dither); // convert the analog value from the ADC's(the joysticks) to the correct PWM duty cycle
 int calculateDither(int dither);           // update the dither value(this value is added to the PWM dutycycle)
 
 #define joyL A2   // FX1056
 #define joyR A7   // FX1057
-#define button 3  // SW3604
-#define MA9136 0  // K1
+#define button 3  // SW3604 (button on joyL)
+#define MA9136 0  // K1 (turned on by button on joyL)
 #define PWM9145 5 // OUT2(U4,Q2)
 #define PWM9146 6 // OUT3(U5,Q4)
 #define PWM9123 8 // OUT1(U1,Q9)
-#define MA9190 1  // K2
+#define MA9190 1  // K2 (invert hydraulic flow from PWM9123)
 
 int ditherRange = 50; // dither range around the PWM dutycycle(so ditherRange = 10 & PWM = 50 => PWM = 45-55)
 int dutyCycle_4546 = 0;
@@ -27,13 +27,12 @@ void setup()
   pinMode(PWM9123, OUTPUT);
   pinMode(PWM9145, OUTPUT);
   pinMode(PWM9146, OUTPUT);
-  pinMode(10, OUTPUT);
 
-  TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(COM1B1);
-  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(COM1B1); //set PWM outputs from clock1(PWM9145&9146) in fast PWM mode
+  TCCR1B = _BV(CS11) | _BV(WGM12);                 //and set clock prescaler zo that PWM freq= around 2KHz closest = 3.8KHz
 
-  TCCR0A = _BV(COM0A1) | _BV(WGM00);
-  TCCR0B = _BV(CS01);
+  TCCR0A = _BV(COM0A1) | _BV(WGM00);               //do the same but for clock0 but only for outputA(PWM9123);
+  TCCR0B = _BV(CS01);                              //this clock frequency is different therefore PWMfreq = 1.9KHz
 }
 
 void loop()
@@ -65,13 +64,9 @@ void loop()
     OCR1B = dutyCycle_4546;
     OCR1A = 255;
   }
-
-  // static bool relayState = false;
-  // digitalWrite(10, relayState);
-  // relayState = !relayState;
 }
 
-int calculateDither(int dither)
+int calculateDither(int dither)  //ads a triangle waveform to the dutycycle so the valves dont stick when position doesnt change
 {
   static bool increase = true;
   if (dither >= ditherRange)
@@ -97,7 +92,7 @@ int calculateDither(int dither)
 void Button(void)
 {
   static bool MA9136State = false; // give the MA9136 the initial state of OFF
-  static int loopsPassed = 0;      // count how many times this function is called while the button is still being pressed
+  static int loopsPassed = 0;      // stores how many times this function is called while the button is still being pressed
   if (!MA9136State)
   {
     if (!(digitalRead(button))) // if the button is LOW(pushed)
@@ -116,10 +111,10 @@ void Button(void)
   }
 }
 
-bool analog2PWM(int whichADC, int dither)
+bool analog2PWM(int whichADC, int dither)  //for more clarity see pictures on trello(libacon volvo)
 {
-  const long maxValue = 511 - 10;
-  const long minValue = 0 + 165;
+  const long maxValue = 511 - 15; //the joystick doesnt always rest in the same mid position so this is to create a deadzone in which the ADC value is ignored and PWM255 is written
+  const long minValue = 0 + 165;  //when the joysticj is fully turned to one side it outputs 0.7V or 4.3 so another deadzone of 0.7V/0.00488V
   const long maxPWM = 255;
   const long minPWM = 0;
   long dutyCycle = 0;
@@ -129,20 +124,23 @@ bool analog2PWM(int whichADC, int dither)
   if (analogValue >= 512)
   {
     polarity = true;
-    analogValue = 511 - (analogValue - 512);
+    analogValue = 511 - (analogValue - 512); //map the value so that instead of 0-1023 it goes from 0-512-0
   }
 
-  if (analogValue < minValue)
+  if (analogValue < minValue) //ignore the deadzone and just write minPWM
   {
     dutyCycle = minPWM;
   }
-  else if (analogValue > maxValue)
+  else if (analogValue > maxValue) //ignore the deadzone and just write maxPWM
   {
     dutyCycle = maxPWM;
   }
-  else
+  else//now the ADCvalue is filtered so that it only can be 165(minvalue)-496(maxvalue)
   {
+    //the next formula maps the value 165-496 to a PWM value of 0-255
+    //dutycycle = (value/totalvalue*maxPWM)+dithervalue
     dutyCycle = ((((analogValue - minValue) * maxPWM) / (maxValue - minValue))) + ((ditherRange / 2) - dither);
+    //because dither gets added and substracted after the value can get out of limit and overflow so check if that happens and limit the value
     if (dutyCycle > 255)
     {
       dutyCycle = 255;
